c4a79e92cb3cfec2ef86b30ae1b85519de2fb15f
alejandro jaramillo <ajaramillo@localhost.localdomain>
2017-11-03 14:36:57 -0400

    Changed the profile table to work with devise user table

--- db/migrate/20171018020008_remove_lockable_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveLockableFromUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :failed_attempts
-    remove_column :users, :unlock_token
-    remove_column :users, :locked_at
-
-  end
-end
