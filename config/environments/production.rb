9ef8d6f903f0e34001de7dedf459ea5ab282bccb
alejandro jaramillo <ajaramillo@localhost.localdomain>
2017-11-03 12:41:48 -0400

    Revert "changing sendgrid username/password for testing 2"
    This reverts commit 2229d39661cb32c8c03e9b654a9f3206d5ae10be.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,8 +97,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['alejandroj1234@heroku.com'],
-      :password       => ENV['@Lph@357!'],
+      :user_name      => alejandroj1234@heroku.com,
+      :password       => @Lph@357!,
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
